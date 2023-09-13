803c4d6fc1df5b65784e41082c89eba80ad45212
Matheus Kenzo <100180936+Dobat00@users.noreply.github.com>
2023-09-13 13:52:23 -0300

    tabuleiro criado

--- ticTacToe/src/App.jsx
+++ ticTacToe/src/App.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import { Board } from "./components/square";
 
-function App() {}
+function App() {
+  return (
+    <div className="content">
+      <Board />
+    </div>
+  );
+}
 
 export default App;
